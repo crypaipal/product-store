4c1b1f6d8898df58a1973e6918311448fba14f99
crypaipal <msamokar228@gmail.com>
2025-03-16 01:11:47 +0100

    Product Shop App completed

---
+++ frontend/src/store/product.js
@@ -0,0 +1,62 @@
+import { create } from "zustand";
+
+export const useProductStore = create((set) => ({
+	products: [],
+	setProducts: (products) => set({ products }),
+	createProduct: async (newProduct) => {
+		if (!newProduct.name || !newProduct.image || !newProduct.price) {
+			return { success: false, message: "Fill all fields, please!" };
+		}
+		else if(newProduct.price < 0) {
+			return { success: false, message: "Price should be a positive number" };
+		}	
+		const res = await fetch("/api/products", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(newProduct),
+		});
+		const data = await res.json();
+		set((state) => ({ products: [...state.products, data.data] }));
+		return { success: true, message: "Product created successfully" };
+	},
+	fetchProducts: async () => {
+		const res = await fetch("/api/products");
+		const data = await res.json();
+		set({ products: data.data });
+	},
+	deleteProduct: async (pid) => {
+		const res = await fetch(`/api/products/${pid}`, {
+			method: "DELETE",
+		});
+		const data = await res.json();
+		if (!data.success) return { success: false, message: data.message };
+
+		set((state) => ({ products: state.products.filter((product) => product._id !== pid) }));
+		return { success: true, message: data.message };
+	},
+	updateProduct: async (pid, updatedProduct) => {
+		if (updatedProduct.price < 0 ) {
+			return { success: false, message: "Price should be a positive number" };
+		}
+		else if (!updatedProduct.price || !updatedProduct.name || !updatedProduct.image ) {
+			return { success: false, message: "Can't leave a blank field" };
+		}
+		const res = await fetch(`/api/products/${pid}`, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(updatedProduct),
+		});
+		const data = await res.json();
+		if (!data.success) return { success: false, message: data.message };
+
+		set((state) => ({
+			products: state.products.map((product) => (product._id === pid ? data.data : product)),
+		}));
+
+		return { success: true, message: data.message };
+	},
+}));
